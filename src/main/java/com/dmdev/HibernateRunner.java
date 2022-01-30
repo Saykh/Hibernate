8485ac0ec51a6d6b51ee3ab8068ccde0af13c182
acer <edilov@mail.ru>
2022-01-30 11:06:49 +0300

    1.14 Практика по жизненному циклу Entity

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -13,49 +13,65 @@ import org.hibernate.cfg.Configuration;
 
 import java.time.LocalDate;
 
+
+import com.dmdev.converter.BirthdayConverter;
+import com.dmdev.entity.Birthday;
+import com.dmdev.entity.Role;
+import com.dmdev.entity.User;
+import com.dmdev.util.HibernateUtil;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
 public class HibernateRunner {
 
     public static void main(String[] args) {
 
 
-        Configuration configuration = new Configuration();
-        configuration.configure();
 
-        configuration.addAnnotatedClass(User.class);
-
-        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-
-        configuration.addAttributeConverter(BirthdayConverter.class, true);
-
-        configuration.registerTypeOverride(new JsonBinaryType());
-
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-
-            session.beginTransaction();
-
-            User user = User.builder()
-                    .username("edilova_at@mail.ru")
-                    .firstname("Albina")
-                    .lastname("Edilova")
-                    .birthDate(new Birthday(LocalDate.of(1993,4,8)))
-                    .role(Role.ADMIN)
-                    .info("""
-                            {   
-                                "name": "Albina",
-                                "nick": "Khedar"
+        User user = User.builder()
+                .username("musaev_mv@mail.ru")
+                .firstname("Mansur")
+                .lastname("Musaev")
+                .birthDate(new Birthday(LocalDate.of(1993,8,8)))
+                .role(Role.ADMIN)
+                .info("""
+                            {
+                                "name": "Mansur",
+                                "nick": "Mans"
                             }
                             """)
-                    .build();
+                .build();
 
 
-            User userGet1 = session.get(User.class, "edilova_at@mail.ru");
-            // User userGet2= session.get(User.class, "edilova_at@mail.ru");
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
 
-            userGet1.setLastname("Edilova");
-            session.flush();
+            try(Session session1 = sessionFactory.openSession()) {
 
-            session.getTransaction().commit();
+                session1.beginTransaction();
+
+                session1.saveOrUpdate(user);
+
+
+
+                session1.getTransaction().commit();
+            }
+
+
+            try(Session session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
+
+                user.setFirstname("Ramzan");
+                session2.merge(user);
+
+
+                session2.getTransaction().commit();
+            }
+
 
         }
 
---
+++ src/main/java/com/dmdev/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package com.dmdev.util;
+
+import com.dmdev.converter.BirthdayConverter;
+import com.dmdev.entity.User;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+
+    public static SessionFactory buildSessionFactory() {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        configuration.addAnnotatedClass(User.class);
+
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+
+        configuration.addAttributeConverter(BirthdayConverter.class, true);
+
+        configuration.registerTypeOverride(new JsonBinaryType());
+
+        return  configuration.buildSessionFactory();
+    }
+}
+
+
