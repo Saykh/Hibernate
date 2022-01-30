8531fe71de509df4f10402d80dfcfca40936bff2
acer <edilov@mail.ru>
2022-01-30 12:52:11 +0300

    2.1-2.2 Логгирование

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -6,8 +6,7 @@ import com.dmdev.entity.Birthday;
 import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
@@ -24,55 +23,52 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 public class HibernateRunner {
 
+    private static final Logger logger = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
 
-
-
         User user = User.builder()
-                .username("musaev_mv@mail.ru")
-                .firstname("Mansur")
-                .lastname("Musaev")
-                .birthDate(new Birthday(LocalDate.of(1993,8,8)))
+                .username("isaeva_au@mail.ru")
+                .firstname("Aska")
+                .lastname("Isaeva")
+                .birthDate(new Birthday(LocalDate.of(1963,11,8)))
                 .role(Role.ADMIN)
                 .info("""
                             {
-                                "name": "Mansur",
-                                "nick": "Mans"
+                                "name": "Aska",
+                                "nick": "Tiger"
                             }
                             """)
                 .build();
+        logger.info("User entity is in transient static, object: {}", user);
 
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            Session session1 = sessionFactory.openSession();
 
-            try(Session session1 = sessionFactory.openSession()) {
+            try(session1) {
 
-                session1.beginTransaction();
+                Transaction transaction = session1.beginTransaction();
+                logger.trace("Transaction is created, {}", transaction);
+
 
                 session1.saveOrUpdate(user);
-
-
+                logger.trace("User is in persistence state: {}, session {}", user, session1);
 
                 session1.getTransaction().commit();
             }
+                logger.warn("User is in detached state: {}, session is closed {}", user, session1);
 
-
-            try(Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setFirstname("Ramzan");
-                session2.merge(user);
-
-
-                session2.getTransaction().commit();
-            }
-
-
+        } catch (Exception exception) {
+            logger.error("Exception occurred", exception);
+            throw exception;
         }
 
     }
